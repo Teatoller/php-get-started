83660dfc609e9077008417aa03d9497ab28cddfc
Teatoller <steven.otieno1@gmail.com>
2019-03-12 13:48:14 +0300

    (form): implement server side php
    - add server side php code to process form field
    
    (#006)

--- form.php
+++ form.php
@@ -9,6 +9,25 @@
     <script src="main.js"></script>
 </head>
 <body>
+    <?php
+    if (isset($_POST['submit'])) {
+
+        printf( 'Username: %s
+        <br>Password: %s
+        <br>Gender: %s
+        <br>Color: %s
+        <br>Language(s): %s,
+        <br>Comments: %s
+        <br>T&amp;C: %s', 
+            $_POST['name'],
+            $_POST['Password'],  
+            $_POST['gender'], 
+            $_POST['color'], 
+            implode(' ', $_POST['languages']), 
+            $_POST['comments'], 
+            $_POST['tc']);
+        }
+    ?>
     <form method="post" action="">
     Username: <input type="text" name="name"><br>
     Password: <input type="password" name="password"><br>
@@ -23,7 +42,7 @@
     Languages spoken:
     <select name="languages[]" multiple size="3">
     <option value="en">English</option>
-    <option value="#fr">French</option>
+    <option value="fr">French</option>
     <option value="it">Italian</option>
     </select><br>
     Comments: <textarea name="comments"></textarea><br>
