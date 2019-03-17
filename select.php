6129ae181bc28f55a9a685986bc0369d7e715c94
Teatoller <steven.otieno1@gmail.com>
2019-03-17 16:47:58 +0300

    (read db): implement select query to db
    - connect to db
    
    - query db
    
    - assign output to variable $result
    
    - iterate over the output
    
    - assign output to place holders
    
    - escape specia characters
    
    - close db
    
    (#014)

--- select.php
+++ select.php
@@ -10,7 +10,25 @@
 </head>
 <body>
     <ul>
-    <li><span style="color: blue;" > Nigel (male) </span></li>
+    <?php
+    $db = mysqli_connect(
+        'localhost',
+        'root',
+        'root',
+        'php'
+    );
+    $sql = 'SELECT * FROM users';
+    $result = mysqli_query($db, $sql);
+    
+    foreach ($result as $row) {
+        printf('<li><span style="color: %s;" > %s (%s) </span></li>',
+        htmlspecialchars($row['color']),
+        htmlspecialchars($row['name']),
+        htmlspecialchars($row['gender'])
+    );
+    }
+    mysqli_close($db);
+    ?>
     </ul>
 </body>
 </html>
