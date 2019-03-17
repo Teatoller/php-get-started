c6c04129f1c8ec0facaac1e3edd90d304203c0ad
Teatoller <steven.otieno1@gmail.com>
2019-03-17 18:36:30 +0300

    (data update): implement data updata function
    - add method to select user by id
    
    - add method to retrieve data from db
    
    - add method to update data from db
    
    - close db
    
    (#015)

--- select.php
+++ select.php
@@ -20,11 +20,13 @@
     $sql = 'SELECT * FROM users';
     $result = mysqli_query($db, $sql);
     
-    foreach ($result as $row) {
-        printf('<li><span style="color: %s;" > %s (%s) </span></li>',
+    foreach($result as $row) {
+        printf('<li><span style="color: %s;" > %s (%s) </span>
+        <a href="update.php?id=%s">edit</a></li>',
         htmlspecialchars($row['color']),
         htmlspecialchars($row['name']),
-        htmlspecialchars($row['gender'])
+        htmlspecialchars($row['gender']),
+        htmlspecialchars($row['id'])
     );
     }
     mysqli_close($db);
